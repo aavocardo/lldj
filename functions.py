2ceac781c1065334e909b19a716f501d3dbd009a
Ricardo Saad <ricardo.saad@proton.me>
2023-01-04 14:25:00 +0200

    good fixes to github please

--- functions.py
+++ functions.py
@@ -1,4 +1,22 @@
 from statistics import mean
+from re import search
+import requests
+
+
+def collect(draw_number: int) -> list:
+    base_host: str = "https://www.indexoflebanon.com/lottery/loto/draw/"
+    url: str = f'{base_host}{str(draw_number)}'
+
+    response = requests.get(url, 'html.parser')
+    response.raise_for_status()
+    source: str = response.text
+
+    string_value = lambda value: f'{value:02}'  # NOQA E731
+
+    results: list = [j for i in range(1, 7) for j in range(1, 43)
+                     if search(f'<div class="loto_no_r bbb{str(i)}">{string_value(j)}</div>', source)]
+
+    return results
 
 
 def precision(predictions: list, targets: list):
@@ -7,29 +25,9 @@ def precision(predictions: list, targets: list):
             print('ligma')
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def main() -> None:     # NOQA
+    print(collect(2050))
+
     prediction_ = [5, 10, 15, 20, 25, 30]
     target_ = [50, 100, 150, 200, 250, 300]
 
