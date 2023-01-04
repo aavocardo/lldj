dff155b6d320c0d4f0d940ec272e62b049070405
Ricardo Saad <ricardo.saad@proton.me>
2023-01-04 02:27:52 +0200

    lesgo

--- compare_accuracy.py
+++ compare_accuracy.py
@@ -1,36 +1,29 @@
 from statistics import mean
 
+# READABLE:
+# prediction = [7, 39, 20, 30]
+# target = [35, 37, 40, 21]
+
 
 def precision(predictions: list, targets: list) -> None:
-    for prediction in range(len(predictions)):
-        for target in range(len(targets)):
-            print(f'{target=}')
-        print(f'{prediction=}')
+    a, b = set(predictions), set(targets)
+    exact_match: set = a.intersection(b)
 
 
 
 
 
 
+    print(f'{exact_match=}\n{predictions=}\n{targets=}')        # NOQA
 
 
-
-
-
-
-
-
-
-
-
-def main() -> None:         # NOQA
-
-    prediction_: list = [7, 15, 19, 26, 32, 40]
+def main() -> None:  # NOQA
+    prediction_: list = [7, 14, 20, 26, 30, 39]
     target_: list = [14, 21, 26, 35, 37, 40]
 
     precision(prediction_, target_)
 
-    print(mean(prediction_))
+    print(round(mean(prediction_)))
 
 
 if __name__ == '__main__':
---
+++ functions.py
@@ -0,0 +1,40 @@
+from statistics import mean
+
+
+def precision(predictions: list, targets: list):
+    for i in range(len(predictions)):
+        for j in range(len(targets)):
+            print('ligma')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main() -> None:     # NOQA
+    prediction_ = [5, 10, 15, 20, 25, 30]
+    target_ = [50, 100, 150, 200, 250, 300]
+
+    precision(prediction_, target_)
+
+
+if __name__ == '__main__':
+    main()
