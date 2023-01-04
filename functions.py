ddef05bb26b73ed5df99147c036a34f26f5f4fbf
Ricardo Saad <ricardo.saad@proton.me>
2023-01-04 15:43:51 +0200

    spacing fix

--- functions.py
+++ functions.py
@@ -7,14 +7,15 @@ def collect(draw_number: int) -> list:
     base_host: str = "https://www.indexoflebanon.com/lottery/loto/draw/"
     url: str = f'{base_host}{str(draw_number)}'
 
-    response = requests.get(url, 'html.parser')
+    response = requests.get(url, 'html.parser', timeout=10)
     response.raise_for_status()
     source: str = response.text
 
     string_value = lambda value: f'{value:02}'  # NOQA E731
 
     results: list = [j for i in range(1, 7) for j in range(1, 43)
-                     if search(f'<div class="loto_no_r bbb{str(i)}">{string_value(j)}</div>', source)]
+                     if search(f'<div class="loto_no_r bbb{str(i)}">'
+                               f'{string_value(j)}</div>', source)]
 
     return results
 
@@ -25,7 +26,7 @@ def precision(predictions: list, targets: list):
             print('ligma')
 
 
-def main() -> None:     # NOQA
+def main() -> None:
     print(collect(2050))
 
     prediction_ = [5, 10, 15, 20, 25, 30]
