cb4b95ce636abbd90130ec2aa14a8b395bf96bab
Ricardo Saad <ricardo.saad@proton.me>
2023-01-05 21:21:43 +0200

    cleaning project files

--- project_functions.py
+++ project_functions.py
@@ -18,6 +18,51 @@ def collect(draw_number: int) -> list:
     return results
 
 
+def compare_precision(prediction, target) -> float:     # DEPRECATING
+    prediction_: list = prediction
+    target_: list = target
+    deviation: list = []
+    result_: list = []
+    _result: list = []
+
+    def average(arg) -> int:
+        total = sum(arg)
+        avg = total/len(arg)
+        return avg
+
+    for i in range(len(prediction_)):
+        if prediction_[i] <= target_[i]:
+            temp = str(target_[i] - prediction_[i])
+            deviation.append(f'±{temp}')
+        else:
+            if prediction_[i] >= target_[i]:
+                temp = str(prediction_[i] - target_[i])
+                deviation.append(f'±{temp}')
+
+    for j in range(len(prediction_)):
+        precision_ = str(int(abs((((target_[j]-prediction_[j])/target_[j])*100)-100)))
+        _result.append(f'{precision_}%')
+        result_.append(int(precision_))
+
+    average_precision: int = round(average(result_))
+    print(f'Precision = {average_precision}%')
+
+    print('\nPrediction:')
+    print(*prediction_, sep=' - ')
+
+    print('\nTarget:')
+    print(*target_, sep=' - ')
+
+    # print('\nDeviation:')
+    # print(*deviation, sep=' - ')
+
+    # final.append(f'{average_precision}%')
+    final = average_precision/100
+    # final.extend(deviation)
+
+    return final
+
+
 def precision(predictions: list, targets: list):
     def proximity_sort(list1: list, list2: list) -> tuple:
         zipped_lists = zip(list1, list2)
