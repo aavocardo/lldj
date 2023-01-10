fd2217e7237aeb2ba87c4819ef5688712e1e4070
Ricardo Saad <ricardo.saad@proton.me>
2023-01-10 17:28:41 +0200

    again

--- main.py
+++ main.py
@@ -23,44 +23,37 @@ def collect(draw_number: int) -> list:
     return results
 
 
-# def compare_precision(prediction, target) -> float:     # DEPRECATED
-#     prediction_: list = prediction
-#     target_: list = target
-#     deviation: list = []
-#     result_: list = []
-#     _result: list = []
-#
-#     for i in range(len(prediction_)):
-#         if prediction_[i] <= target_[i]:
-#             temp = str(target_[i] - prediction_[i])
-#             deviation.append(f'±{temp}')
-#         else:
-#             if prediction_[i] >= target_[i]:
-#                 temp = str(prediction_[i] - target_[i])
-#                 deviation.append(f'±{temp}')
-#
-#     for j in range(len(prediction_)):
-#         precision_ = str(int(abs((((target_[j]-prediction_[j])/target_[j])*100)-100)))
-#         _result.append(f'{precision_}%')
-#         result_.append(int(precision_))
-#
-#     average_precision: int = round(average(result_))
-#     print(f'Precision = {average_precision}%')
-#
-#     print('\nPrediction:')
-#     print(*prediction_, sep=' - ')
-#
-#     print('\nTarget:')
-#     print(*target_, sep=' - ')
-#
-#     # print('\nDeviation:')
-#     # print(*deviation, sep=' - ')
-#
-#     # final.append(f'{average_precision}%')
-#     final = average_precision/100
-#     # final.extend(deviation)
-#
-#     return final
+def compare_precision(prediction, target) -> float:     # DEPRECATED
+    prediction_: list = prediction
+    target_: list = target
+    deviation: list = []
+    result_: list = []
+    _result: list = []
+
+    for i in range(len(prediction_)):
+        if prediction_[i] <= target_[i]:
+            temp = str(target_[i] - prediction_[i])
+            deviation.append(f'±{temp}')
+        else:
+            if prediction_[i] >= target_[i]:
+                temp = str(prediction_[i] - target_[i])
+                deviation.append(f'±{temp}')
+
+    for j in range(len(prediction_)):
+        precision_ = str(int(abs((((target_[j]-prediction_[j])/target_[j])*100)-100)))
+        _result.append(f'{precision_}%')
+        result_.append(int(precision_))
+
+    average_precision: int = round(average(result_))
+    print(f'Precision = {average_precision}%')
+
+    print('\nPrediction:')
+    print(*prediction_, sep=' - ')
+
+    print('\nTarget:')
+    print(*target_, sep=' - ')
+
+    return average_precision/100
 
 
 def debug_generator(range_min: int, range_max: int, count: int) -> tuple:
