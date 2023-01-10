c072b3cf330b1b81105f92abe4256da7f3204027
Ricardo Saad <ricardo.saad@proton.me>
2023-01-10 10:33:37 +0200

    again

--- main.py
+++ main.py
@@ -10,20 +10,6 @@ def string_value(value: int) -> str:
     return f'{value:02}'
 
 
-# def proximity_sort(list1: list, list2: list) -> tuple:
-#     zl = zip(list1, list2)
-#     zl = sorted(zl, key=lambda x: abs(x[0] - x[1]))
-#     list1, list2 = zip(*zl)
-#     return list(list1), list(list2)
-
-
-def proximity_sort(list1: list, list2: list) -> tuple:
-    zl = zip(list1, list2)
-    zl = sorted(zl, key=lambda x: (abs(x[0] - x[1]), x[0]))
-    list1, list2 = zip(*zl)
-    return list(list1), list(list2)
-
-
 def collect(draw_number: int) -> list:
     url: str = f'https://www.indexoflebanon.com/lottery/loto/draw/{str(draw_number)}'
     response = requests.get(url, 'html.parser', timeout=10)
@@ -36,48 +22,55 @@ def collect(draw_number: int) -> list:
     return results
 
 
-def compare_precision(prediction, target) -> float:     # DEPRECATING
-    prediction_: list = prediction
-    target_: list = target
-    deviation: list = []
-    result_: list = []
-    _result: list = []
+# def compare_precision(prediction, target) -> float:     # DEPRECATED
+#     prediction_: list = prediction
+#     target_: list = target
+#     deviation: list = []
+#     result_: list = []
+#     _result: list = []
+#
+#     for i in range(len(prediction_)):
+#         if prediction_[i] <= target_[i]:
+#             temp = str(target_[i] - prediction_[i])
+#             deviation.append(f'±{temp}')
+#         else:
+#             if prediction_[i] >= target_[i]:
+#                 temp = str(prediction_[i] - target_[i])
+#                 deviation.append(f'±{temp}')
+#
+#     for j in range(len(prediction_)):
+#         precision_ = str(int(abs((((target_[j]-prediction_[j])/target_[j])*100)-100)))
+#         _result.append(f'{precision_}%')
+#         result_.append(int(precision_))
+#
+#     average_precision: int = round(average(result_))
+#     print(f'Precision = {average_precision}%')
+#
+#     print('\nPrediction:')
+#     print(*prediction_, sep=' - ')
+#
+#     print('\nTarget:')
+#     print(*target_, sep=' - ')
+#
+#     # print('\nDeviation:')
+#     # print(*deviation, sep=' - ')
+#
+#     # final.append(f'{average_precision}%')
+#     final = average_precision/100
+#     # final.extend(deviation)
+#
+#     return final
 
-    for i in range(len(prediction_)):
-        if prediction_[i] <= target_[i]:
-            temp = str(target_[i] - prediction_[i])
-            deviation.append(f'±{temp}')
-        else:
-            if prediction_[i] >= target_[i]:
-                temp = str(prediction_[i] - target_[i])
-                deviation.append(f'±{temp}')
 
-    for j in range(len(prediction_)):
-        precision_ = str(int(abs((((target_[j]-prediction_[j])/target_[j])*100)-100)))
-        _result.append(f'{precision_}%')
-        result_.append(int(precision_))
-
-    average_precision: int = round(avg(result_))
-    print(f'Precision = {average_precision}%')
-
-    print('\nPrediction:')
-    print(*prediction_, sep=' - ')
-
-    print('\nTarget:')
-    print(*target_, sep=' - ')
-
-    # print('\nDeviation:')
-    # print(*deviation, sep=' - ')
-
-    # final.append(f'{average_precision}%')
-    final = average_precision/100
-    # final.extend(deviation)
-
-    return final
+def proximity_sort(list1: list, list2: list) -> tuple:
+    zl = zip(list1, list2)
+    zl = sorted(zl, key=lambda x: (abs(x[0] - x[1]), x[0]))
+    list1, list2 = zip(*zl)
+    return list(list1), list(list2)
 
 
-def precision(predictions: list, targets: list):
-    exact_match = list(filter(lambda y: y in predictions, targets))
+def precision(predictions: list, targets: list) -> None:
+    exact_match = list(filter(lambda x: x in predictions, targets))
     predictions: list = [item for item in predictions if item not in exact_match]
     targets: list = [item for item in targets if item not in exact_match]
 
@@ -92,12 +85,7 @@ def main() -> None:
     p1 = [6, 12, 18, 23, 30, 36]
     t1 = [14, 21, 26, 23, 30, 35]
 
-    p2 = [5, 10, 15, 20, 25, 30]
-    t2 = [50, 100, 150, 200, 250, 300]
-
-    # precision(p1, t1)
-
-    p, t = proximity_sort([6, 12, 18, 36], [14, 21, 26, 35])
+    p, t = proximity_sort(p1, t1)
 
     print(f'{p=}\n{t=}')
 
