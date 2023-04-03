f9558643776ebc77bbfe0e841bd57e628576f8d3
Ricardo Saad <ricardo.saad@proton.me>
2023-04-04 01:47:40 +0300

    draw 2097 updated, prediction failed :(

--- main.py
+++
@@ -1,6 +0,0 @@
-from modules import Lottery
-
-
-for i in range(2094, 2097):
-    x = Lottery(i)
-    x.to_csv()
---
+++ scratch.py
@@ -0,0 +1,4 @@
+from modules import Lottery
+
+x: Lottery = Lottery(2097)
+x.to_csv()
